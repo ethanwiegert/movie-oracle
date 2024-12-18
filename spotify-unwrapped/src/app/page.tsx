6aaa467ea76c9b4eaa51a4bd7a485ddd0dbfcad5
Ethan Wiegert <ewiegert99@gmail.com>
2024-12-18 18:29:31 -0500

    Navbar and initial home design.

--- spotify-unwrapped/src/app/page.tsx
+++ spotify-unwrapped/src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Animation() {
   
   return (
     
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
                 
       <main className="flex flex-col gap-8 row-start-2 items-center">
        
@@ -41,7 +41,7 @@ export default function Animation() {
             </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="row-start-3 items-center justify-center absolute bottom-0 m-4">
         
     
           <p>Spotify Unwrapped - Ethan Wiegert</p>
