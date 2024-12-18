0879d4132e40c235e5881cbca0e913504aca6c49
Ethan Wiegert <ewiegert99@gmail.com>
2024-12-18 18:49:36 -0500

    Updates to home page layout.

--- spotify-unwrapped/src/app/page.tsx
+++ spotify-unwrapped/src/app/page.tsx
@@ -19,25 +19,25 @@ export default function Animation() {
   
   return (
     
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center pt-40 font-[family-name:var(--font-geist-sans)]">
                 
-      <main className="flex flex-col gap-8 row-start-2 items-center">
+      <main className="flex flex-col gap-8 row-start-1 items-center">
        
       <h1>Spotify Unwrapped</h1>
       
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-        <h3><i>Open up to your future</i></h3>
+        <h2><i>Open up to your future</i></h2>
        
         </div>
-        <div className="flex flex-row">
+        <div className="flex flex-row pt-16">
             <div id="animation">
             {displayNote ? 
-            <svg id="display-note" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="animate-bounce h-12 w-12">
+            <svg id="display-note" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="animate-bounce h-24 w-24">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m9 9 10.5-3m0 6.553v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 1 1-.99-3.467l2.31-.66a2.25 2.25 0 0 0 1.632-2.163Zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 0 1-.99-3.467l2.31-.66A2.25 2.25 0 0 0 9 15.553Z" />
             </svg>
             : 
-            <div id = "box-open" className="bg-black h-12 w-12 home-box home-box-open"></div>}
+            <div id = "box-open" className="bg-black h-24 w-24 home-box home-box-open"></div>}
             </div>
         </div>
       </main>
