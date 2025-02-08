a2fb06b47088172bfbda10098685e776d7de570e
Ethan Wiegert <ewiegert99@gmail.com>
2025-02-08 10:32:07 -0500

    Initial work on authentication.

--- spotify-unwrapped/src/app/profile/page.tsx
+++ spotify-unwrapped/src/app/profile/page.tsx
@@ -1,25 +1,40 @@
 'use client'
-//import { useEffect } from "react";
-
+import { useEffect } from "react";
+import { SpotifyApi } from '@spotify/web-api-ts-sdk';
 
 export default function Profile(){
 
     // Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
-    /*useEffect(()=>{
-        const response = fetch('https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb', {
-            headers: {
-              Authorization: 'Bearer ' + ""
-            }
-          })
-        .then(response => response.json())
-        .then(data => console.log(data));
-    })*/
+
+
+    async function GetAuthorization(){
+      const ClientID =  process.env.Spotify_Client
+      if(ClientID!=null){
+        const sdk = SpotifyApi.withUserAuthorization(ClientID.toString(), "https://localhost:3000", ["user-read-recently-played"]);
+        return sdk
+      }
+      
+    }
+
+    async function GetUser(){
+      const sdk = await GetAuthorization();
+      if(sdk != null){
+        const user = await sdk.currentUser.profile()
+        return user
+      }
+    }
+
+    useEffect(()=>{
+      const user = GetUser();
+      console.log(user)
+    })
     
 
     
     return (
     <>
-<h1>Profile data</h1>
+<h1>Test Artist Data</h1>
+map()
     </>
 )
 }
